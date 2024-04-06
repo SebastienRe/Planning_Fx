1d89d0b4360da21f3c193993683803703b44dd37
Sebastien <sebastien.re@alumni.univ-avignon.fr>
2024-04-06 14:26:26 +0200

    ajustements visuels

--- src/main/java/com/ceri/planningfx/controller/ConnexionController.java
+++ src/main/java/com/ceri/planningfx/controller/ConnexionController.java
@@ -69,7 +69,7 @@ public class ConnexionController {
             this.clearLabel();
 
             if (AccountService.connexion(username.getText(), password.getText())) {
-                Router.changeScene("main.fxml", 930, 800);
+                Router.changeScene("main.fxml", 1000, 690);
             } else {
                 infoLabel.setText("Connexion échouée");
             }
